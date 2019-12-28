f2d03e98b0f8b3305870d2b6a160a89f8d018f9d
cristianagrigoriu <cristiana.grigoriu@gmail.com>
2019-12-28 14:02:23 +0200

    Added template for data edit - user login

---
+++ src/main/java/com/cris/mavenmultidepot/Views/UserLoginView.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cris.mavenmultidepot.Views;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import org.primefaces.PrimeFaces;
+
+/**
+ *
+ * @author cristiana
+ */
+@Named
+@ManagedBean
+@RequestScoped
+public class UserLoginView {
+     
+    private String username;
+     
+    private String password;
+ 
+    public String getUsername() {
+        return username;
+    }
+ 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+   
+    public void login() {
+        int x = 10;
+        FacesMessage message = null;
+        boolean loggedIn = false;
+         
+        if(username != null && username.equals("admin") && password != null && password.equals("admin")) {
+            loggedIn = true;
+            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
+        } else {
+            loggedIn = false;
+            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
+        }
+         
+        FacesContext.getCurrentInstance().addMessage(null, message);
+        PrimeFaces.current().ajax().addCallbackParam("loggedIn", loggedIn);
+    }   
+}
